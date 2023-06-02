3f9df6d4d1715a5a973c01518feb1e2541744580
jai <jai.bhalla2021@vitstudent.ac.in>
2023-06-02 11:37:15 +0530

    intial commit

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../images/profile.jpg'
+import Image from "next/image";
+
+const Navbar = () => {
+    return (
+        <>
+            navabr
+        </>
+    );
+};
+
+export default Navbar;
